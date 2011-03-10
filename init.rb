ecae9c26d9aa6074fbdd2ff149602f5de992f14c
Jens Ulferts <j.ulferts@finn.de>
2011-03-10 14:41:05 +0100

    version to 0.2.0

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_announcements do
   name 'Redmine Announcements plugin'
   author 'Jens Ulferts @ Finnlabs'
   description 'Adds announcements to the login page which are configurable for admins'
-  version '0.1.0'
+  version '0.2.0'
 
   menu :admin_menu, :announcements,
       {:controller => 'announcements', :action => 'edit'},
