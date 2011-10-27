7ee776f5582f32f18a49777f982fdc86db7a59e2
Gregor Schmidt <ruby@schmidtwisser.de>
2011-10-27 09:16:30 +0200

    tabs -> spaces

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,13 +1,13 @@
 module AnnouncementsHelper
   def announcement_date_field_with_calendar(interval, field_name)
-		label_name = "label_" + field_name
-		ret = "<label>#{l(label_name.to_sym)}</label>"
+    label_name = "label_" + field_name
+    ret = "<label>#{l(label_name.to_sym)}</label>"
 
-		ret += text_field_tag "announcement[#{field_name}]", interval.strftime("%Y-%m-%d"), :size => 10, :name => "announcement[#{field_name}]"
+    ret += text_field_tag "announcement[#{field_name}]", interval.strftime("%Y-%m-%d"), :size => 10, :name => "announcement[#{field_name}]"
     ret += label_tag "announcement_#{field_name}"
 
-		ret += calendar_for("announcement_#{field_name}")
+    ret += calendar_for("announcement_#{field_name}")
 
-		ret
-	end
+    ret
+  end
 end
