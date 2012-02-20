67cb0bfdde7d81c451f2053cdfed61200b276ba6
Jens Ulferts <j.ulferts@finn.de>
2012-02-20 15:13:05 +0100

    cukefix: apdapting paths to altered routes

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,15 +1,12 @@
-module NavigationHelpers
-  def path_to_with_announcements(page_name)
+module AnnoucementNavigationHelpers
+  def path_to(page_name)
     case page_name
-
     when /the edit page of Announcement/
-      '/announcements/1/edit'
+      '/admin/announcement/edit'
     else
-      path_to_without_announcements(page_name)
+      super
     end
   end
-
-  alias_method_chain :path_to, :announcements
 end
 
-World(NavigationHelpers)
+World(AnnoucementNavigationHelpers)
