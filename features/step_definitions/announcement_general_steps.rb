64863b5cf98f9e0a34fad8521cb47cdb59601002
Michael Frister <michael+git@frister.net>
2013-08-05 14:53:45 +0100

    Fix cukes

--- features/step_definitions/announcement_general_steps.rb
+++ features/step_definitions/announcement_general_steps.rb
@@ -1,5 +1,5 @@
 Given /^there is an (active|inactive) announcement saying (.+)$/ do |status, text|
   active = status == "active" ? true : false
   text = text.gsub("\"","")
-  Factory.create(:announcement, :text => text, :active => active)
+  FactoryGirl.create(:announcement, :text => text, :active => active)
 end
