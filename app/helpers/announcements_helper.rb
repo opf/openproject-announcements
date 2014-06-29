f4f4455a0c687e30a703d6a0d584b39f864589b1
Alex Coles <alex@alexbcoles.com>
2014-06-30 00:31:40 +0200

    Cleanup whitespace
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -11,9 +11,9 @@ module AnnouncementsHelper
 
   def notice_annoucement_active
     if @announcement.active_and_current?
-			l(:'announcements.is_active')
-		else
-			l(:'announcements.is_inactive')
-		end
+      l(:'announcements.is_active')
+    else
+      l(:'announcements.is_inactive')
+    end
   end
 end
